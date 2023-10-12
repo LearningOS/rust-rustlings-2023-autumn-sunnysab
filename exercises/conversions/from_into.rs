fe07a8dd7fc385926d8c956d5aebaf108c4bce2c
sunnysab <i@sunnysab.cn>
2023-10-12 21:24:49 +0800

    Complete.

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -40,10 +40,26 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of
 // Person Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
 
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        if s.len() == 0 {
+            return Default::default();
+        }
+        
+        match s.split_once(",") {
+            Some((name, age)) => {
+                if name.is_empty() {
+                    return Default::default();
+                }
+
+                match str::parse(age) {
+                    Ok(age) => Person { name: name.to_string(), age },
+                    Err(e) => Default::default(),
+                }
+            },
+            None => Default::default(),
+        }
     }
 }
 
--- exercises/conversions/from_str.rs
+++ exercises/conversions/from_str.rs
@@ -31,7 +31,6 @@ enum ParsePersonError {
     ParseInt(ParseIntError),
 }
 
-// I AM NOT DONE
 
 // Steps:
 // 1. If the length of the provided string is 0, an error should be returned
@@ -52,6 +51,22 @@ enum ParsePersonError {
 impl FromStr for Person {
     type Err = ParsePersonError;
     fn from_str(s: &str) -> Result<Person, Self::Err> {
+        if s.is_empty() {
+            return Err(ParsePersonError::Empty);
+        }
+        let columns = s.split(",").collect::<Vec<_>>();
+        if columns.len() != 2 {
+            return Err(ParsePersonError::BadLen);
+        }
+        if columns[0].is_empty() {
+            return Err(ParsePersonError::NoName);
+        }
+
+        let name = columns[0];
+        let age = str::parse::<usize>(columns[1])
+            .map_err(|e| ParsePersonError::ParseInt(e))?;
+        
+        Ok(Person {name: name.to_string(), age})
     }
 }
 
